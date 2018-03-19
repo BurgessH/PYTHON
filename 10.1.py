2d46ae990831782158e59c7a95b110e665e8b342
HM <2042877026@qq.com>
2018-03-19 16:06:55 +0800

    Update

--- 10.1.py
+++ 10.1.py
@@ -17,6 +17,8 @@ print(a)
 
 ##2.0.在函数中全局变量修改:如果是可变类型可以执行修改变量的值；
 # 不可变类型值是不能修改：本质上来说不可变类型值的修改，只是修改了引用。
+    ## 不可变类型：字符串；元祖
+    ##可变类型：列表、字典
 
 names = ["one", "two", "three"] #列表
 student = {"name":"onex"}       #字典
@@ -27,6 +29,7 @@ def test3():
     print("原始全局变量为： %s"%names)
     names[2] = "four"              #修改全局变量
     student["age"] = 23
+
     global a
     a = "toyota"                
     b =b + 1
@@ -38,4 +41,35 @@ print("现在元祖的元素：%s"%student)
 print("现在字符串的元素：%s"%a)
 print("现在常量的元素：%s"%a)
 
-    
+#3.0.函数返回多个值：
+def test1():
+    a = 1
+    b = 2
+    return a,b
+
+x, y=test1()
+print(x, y)
+
+x=test1()
+print(x) #返回元祖  
+
+##1.缺省参数：
+def test1(x,y,z=10): #带有默认值的参数一定要位于参数列表的最后面
+    return x+y+z
+print("sum = %s"%test1(5))
+
+##2.不定长参数:
+def test2(x,y, *args, **kwargs):
+    print(x,y)
+    print(args)
+    print(kwargs)
+
+    sum = x
+    for i in args:
+        sum+=i
+   
+    for i in kwargs.values():
+        sum+=i
+     print("sum = %s"%sum)
+
+test1(2,3,4,5,6,num1=5, num2=6) #字典表示
